cdcb245fb0166ce680e52e6c21c702ff147a7c8b
Awsome <Awsome@DESKTOP-RC09QPR>
2016-11-27 19:16:01 +0200

    Random Population Generation & Fitness function added - for ground level

--- AutomaticGameLevelGeneration/AutomaticGameLevelGeneration/Form1.cs
+++ AutomaticGameLevelGeneration/AutomaticGameLevelGeneration/Form1.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AutomaticGameLevelGeneration
 {
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -17,4 +12,148 @@ namespace AutomaticGameLevelGeneration
             InitializeComponent();
         }
     }
+
+
+    public class ApplicationLogic
+    {
+        public void Execute()
+        {
+            Random rnd = new Random();
+            List<Individ> individs = new List<Individ>();
+            for (int i = 0; i < 100; i++)
+            {
+                var individ = new Individ(rnd);
+
+                individs.Add(individ);
+            }
+        }    
+    }
+
+    public class FitnessFunction
+    {
+        public static float GetGroundFitness(Individ x)
+        {
+            int[] groundLevelCount = new int[15];
+            for (int i = 0; i < groundLevelCount.Length; i++)
+            {
+                groundLevelCount[i] = 0;
+            }
+
+            for (int i = 0; i < x.groundLevel.Length; i++)
+            {
+                int height = x.groundLevel[i];
+                groundLevelCount[height]++;
+            }
+
+            float[] groundLevelProbabilities = new float[15];
+            for (int i = 0; i < groundLevelProbabilities.Length ; i++)
+            {
+                float probability = (float) groundLevelCount[i]/x.groundLevel.Length;
+                groundLevelProbabilities[i] = probability;
+            }
+
+            float fitness = 0;
+            float sum = 0;
+            for (int i = 0; i < groundLevelProbabilities.Length; i++)
+            {
+                var groundLevelProbability = groundLevelProbabilities[i];
+                var log = Math.Log(groundLevelProbability, 2);
+                sum += groundLevelProbability*(float)log;
+            }
+
+            fitness = -1*sum;
+            return fitness;
+        }
+    }
+
+    public class Individ
+    {
+        public int[] groundLevel;
+
+        public int[] blockType;
+
+        public int[] blockHeight;
+
+        public int[] enemyType;
+
+        public int[] coinHeight;
+
+        public float? fitness = null;
+
+        public Individ(Random rnd)
+        {
+            groundLevel = new int[100];
+            blockType = new int[100];
+            blockHeight = new int[100];
+            enemyType = new int[100];
+            coinHeight = new int[100];
+
+            for (int i = 0; i < 100; i++)
+            {
+                groundLevel[i] = GetRandomGroundLevel(rnd);
+                blockType[i] = GetRandomBlockType(rnd);
+                blockHeight[i] = GetRandomGroundLevel(rnd);
+                enemyType[i] = GetRandomEnemyType(rnd);
+                coinHeight[i] = GetRandomCoinHeight(rnd);
+            }
+        }
+
+        public static int GetRandomGroundLevel(Random rnd)
+        {
+            return rnd.Next(16);
+        }
+
+        public static int GetRandomBlockType(Random rnd)
+        {
+            return rnd.Next(5);
+        }
+
+        public static int GetRandomBlockHeight(Random rnd)
+        {
+            return rnd.Next(1, 5);
+        }
+
+        public static int GetRandomEnemyType(Random rnd)
+        {
+            return rnd.Next(3);
+        }
+
+        public static int GetRandomCoinHeight(Random rnd)
+        {
+            return rnd.Next(11);
+        }
+    }
+
+    //public class Generation
+    //{
+    //    private Population population;
+    //    public static Generation GetGenerationZero()
+    //    {
+    //        throw new System.NotImplementedException();
+    //    }
+    //}
+
+    //public class Population
+    //{
+    //    private List<Individ> individs;
+    //}
+
+    //internal class Individ
+    //{
+    //    private List<Chromosome> chromosomes;
+    //    private float fitness;
+    //}
+
+    //internal class Chromosome
+    //{
+    //    private int groundLevel;
+
+    //    private int blockType;
+
+    //    private int blockHeight;
+
+    //    private int enemyType;
+
+    //    private int coinHeight;
+    //}
 }
