6cb2cb99d3692e10072ee66aafc6d9681a327e9d
sayuri.programmer.girl <sayuri.programmer.girl@gmail.com>
2016-11-28 16:49:30 +0200

    Add sparseness formula

---
+++ AutomaticGameLevelGeneration/AutomaticGameLevelGeneration.Tests/SparsenessFormulaTests.cs
@@ -0,0 +1,16 @@
+﻿namespace AutomaticGameLevelGeneration.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SparsenessFormulaTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            int[] testValues = { 1, 0, 1, 1};
+            double result = new SparesenessFormula(4).Compute(testValues);
+            Assert.AreEqual(2,result);
+        }
+    }
+}
--- AutomaticGameLevelGeneration/AutomaticGameLevelGeneration/Form1.cs
+++ AutomaticGameLevelGeneration/AutomaticGameLevelGeneration/Form1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AutomaticGameLevelGeneration
@@ -11,129 +17,4 @@ namespace AutomaticGameLevelGeneration
             InitializeComponent();
         }
     }
-
-
-    public class ApplicationLogic
-    {
-        public void Execute()
-        {
-            Random rnd = new Random();
-            List<Individ> individs = new List<Individ>();
-            for (int i = 0; i < 100; i++)
-            {
-                var individ = new Individ(rnd);
-
-                individs.Add(individ);
-            }
-        }    
-    }
-
-
-    public class FitnessFunction
-    {
-        public double EvaluateIndividual()
-        {
-            return 0;
-        }
-
-        public double EvaluateGroundLevels(int[] groundLevels)
-        {
-            var entropyFormula = new EntropyFormula(15, 100);
-            var entropy = entropyFormula.Compute(groundLevels);
-
-            double fitness = 0;
-            return fitness;
-        }
-    }
-
-    public class Individ
-    {
-        public int[] groundLevel;
-
-        public int[] blockType;
-
-        public int[] blockHeight;
-
-        public int[] enemyType;
-
-        public int[] coinHeight;
-
-        public float? fitness = null;
-
-        public Individ(Random rnd)
-        {
-            groundLevel = new int[100];
-            blockType = new int[100];
-            blockHeight = new int[100];
-            enemyType = new int[100];
-            coinHeight = new int[100];
-
-            for (int i = 0; i < 100; i++)
-            {
-                groundLevel[i] = GetRandomGroundLevel(rnd);
-                blockType[i] = GetRandomBlockType(rnd);
-                blockHeight[i] = GetRandomGroundLevel(rnd);
-                enemyType[i] = GetRandomEnemyType(rnd);
-                coinHeight[i] = GetRandomCoinHeight(rnd);
-            }
-        }
-
-        public static int GetRandomGroundLevel(Random rnd)
-        {
-            return rnd.Next(16);
-        }
-
-        public static int GetRandomBlockType(Random rnd)
-        {
-            return rnd.Next(5);
-        }
-
-        public static int GetRandomBlockHeight(Random rnd)
-        {
-            return rnd.Next(1, 5);
-        }
-
-        public static int GetRandomEnemyType(Random rnd)
-        {
-            return rnd.Next(3);
-        }
-
-        public static int GetRandomCoinHeight(Random rnd)
-        {
-            return rnd.Next(11);
-        }
-    }
-
-    //public class Generation
-    //{
-    //    private Population population;
-    //    public static Generation GetGenerationZero()
-    //    {
-    //        throw new System.NotImplementedException();
-    //    }
-    //}
-
-    //public class Population
-    //{
-    //    private List<Individ> individs;
-    //}
-
-    //internal class Individ
-    //{
-    //    private List<Chromosome> chromosomes;
-    //    private float fitness;
-    //}
-
-    //internal class Chromosome
-    //{
-    //    private int groundLevel;
-
-    //    private int blockType;
-
-    //    private int blockHeight;
-
-    //    private int enemyType;
-
-    //    private int coinHeight;
-    //}
 }
