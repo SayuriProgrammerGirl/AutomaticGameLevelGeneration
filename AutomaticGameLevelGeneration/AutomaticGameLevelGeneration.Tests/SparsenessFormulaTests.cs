fea20f0c6dd5cb5cacbc980b485c6ffb563a1d8c
sayuri.programmer.girl <sayuri.programmer.girl@gmail.com>
2016-12-04 09:07:42 +0200

    Sparseness formula computation implemented

--- AutomaticGameLevelGeneration/AutomaticGameLevelGeneration.Tests/SparsenessFormulaTests.cs
+++ AutomaticGameLevelGeneration/AutomaticGameLevelGeneration.Tests/SparsenessFormulaTests.cs
@@ -6,7 +6,7 @@
     public class SparsenessFormulaTests
     {
         [TestMethod]
-        public void Test()
+        public void TestOfSparsenessForTestData1()
         {
             int[] testValues = { 1, 0, 1, 1};
             double result = new SparesenessFormula(4).Compute(testValues);
---
+++ AutomaticGameLevelGeneration/AutomaticGameLevelGeneration/SparesenessFormula.cs
@@ -0,0 +1,64 @@
+namespace AutomaticGameLevelGeneration
+{
+    using System;
+
+    public class SparesenessFormula
+    {
+        private readonly int numberOfValues;
+
+        public SparesenessFormula(int numberOfValues)
+        {
+            this.numberOfValues = numberOfValues;
+        }
+
+        //For coins, blocks, enemies
+        public double Compute(int[] values)
+        {
+            var sum = this.GetSum(values);
+
+            double sparseness = sum * this.GetMultiplicationCoefficient();
+
+            return sparseness;
+        }
+
+        private double GetMultiplicationCoefficient()
+        {
+            return (double)2 / (this.numberOfValues * (this.numberOfValues - 1));
+        }
+
+        internal double GetSum(int[] values)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < this.numberOfValues; i++)
+            {
+                if (IsNotEmptyValue(values[i]))
+                {
+                    sum += this.GetSumOfAllDistancesFromValue(i, values);
+                }
+            }
+            return sum;
+        }
+
+        internal double GetSumOfAllDistancesFromValue(int valuePosition, int[] values)
+        {
+            double sum = 0;
+
+            for (int position = 0; position < this.numberOfValues; position++)
+            {
+                if (IsNotEmptyValue(values[position]))
+                {
+                    int distance = Math.Abs(valuePosition - position);
+                    sum += distance;
+                }
+            }
+
+            return sum;
+        }
+
+        internal static bool IsNotEmptyValue(int value)
+        {
+            return value != 0;
+        }
+    }
+}
